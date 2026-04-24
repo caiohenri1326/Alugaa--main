0807b5a46459ca05bf13720dab51c0c2548ccb05
Caio Henrique <caiobr1326@gmail.com>
2026-04-24 12:56:57 -0300

    QUASE DESISTINDO FRONT-END 24/04 12:57

--- front-end/config/script.js
+++ front-end/config/script.js
@@ -6,9 +6,20 @@ const tela_perfil = document.getElementById('container-perfil');
 
 const tela_ativa = document.querySelectorAll('.tela');
 
+const text_area = document.getElementById('text-area');
+
 
 botao_ativar_perfil.addEventListener('click', function(){
-    tela_perfil.classList.toggle('ativa');
+    tela_perfil.classList.add('ativa');
+
+    text_area.innerText = "Perfil"
+
+    tela_perfil.forEach(tela_ativa => {
+        tela_ativa.style.display = 'none';
+    })
+
+    document.querySelectorAll('.nav-item').forEach(btn => btn.classList.remove('ativo'));
+    this.classList.add('ativo');
 })
 
 
@@ -26,3 +37,23 @@ botoesMenu.forEach(botao => {
         this.classList.add('ativo');
     })
 })
+
+
+
+
+
+
+const divisao_container_esquerdo = document.getElementById('divisao-container-esquerdo');
+
+const config_sidebar = document.getElementById('config-sidebar');
+
+const container_exit = document.getElementById('container-exit');
+ 
+divisao_container_esquerdo.addEventListener('click', function(){
+    if(config_sidebar.style.width === "60px"){
+        config_sidebar.style.width = "290px";
+    }else{
+        config_sidebar.style.width = "45px";
+        config_sidebar.style.overflow = "hidden";
+    }
+});
