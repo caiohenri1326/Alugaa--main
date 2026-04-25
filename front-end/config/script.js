91e8ef5a969e8bcfda483a645360713210368c24
caiohenri1326 <caiobr1326@gmail.com>
2026-04-24 21:49:19 -0300

    ATUALIZAÇAO AREA CONFIG 24/04 21:49

--- front-end/config/script.js
+++ front-end/config/script.js
@@ -1,3 +1,6 @@
+const folhaEstilo = document.styleSheets[0];
+
+
 // FAZER APARECER TELA DE PERFIL CONFIGURAÇOES
 
 const botao_ativar_perfil = document.getElementById('botao-ativar-perfil');
@@ -12,7 +15,7 @@ const text_area = document.getElementById('text-area');
 botao_ativar_perfil.addEventListener('click', function(){
     tela_perfil.classList.add('ativa');
 
-    text_area.innerText = "Perfil"
+    text_area.innerText = "Perfil";
 
     tela_perfil.forEach(tela_ativa => {
         tela_ativa.style.display = 'none';
@@ -32,7 +35,7 @@ botoesMenu.forEach(botao => {
     botao.addEventListener('click', function(){
         botoesMenu.forEach(btn => btn.classList.remove('ativo'));
 
-        
+
 
         this.classList.add('ativo');
     })
@@ -49,11 +52,20 @@ const config_sidebar = document.getElementById('config-sidebar');
 
 const container_exit = document.getElementById('container-exit');
  
+const hr_vertical = document.querySelector('.divisao-vertical');
+const hr_sair = document.querySelector('.my-3');
+const container_botoes_env = document.querySelector('.container-botoes-env');
+
 divisao_container_esquerdo.addEventListener('click', function(){
-    if(config_sidebar.style.width === "60px"){
-        config_sidebar.style.width = "290px";
-    }else{
-        config_sidebar.style.width = "45px";
-        config_sidebar.style.overflow = "hidden";
+    config_sidebar.classList.toggle('ativado');
+
+    if(config_sidebar.classList.contains('ativado')){
+        hr_vertical.style.marginLeft = '50px';
+        hr_sair.style.width = '50px';
+        container_botoes_env.style.overflow = 'hidden';
+    }
+    else{
+        hr_vertical.style.marginLeft = '15.2%';
+        hr_sair.style.width = '15%';
     }
 });
